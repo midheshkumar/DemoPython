4c039803c582bc0eea0d92954e493fbd3f5de1c0
Midhesh Kumar <midheshkumaralpy@gmail.com>
2024-01-29 10:52:34 +0530

    added

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib import admin
+from django.contrib import admin
+from django.urls import path,include
+
+from todoapp import views
+
+urlpatterns = [
+
+    path('',views.index,name='index'),
+    path('delete<int:taskid>/',views.delete,name='delete'),
+    path('update<int:id>/',views.update,name='update')
+]
+
+
